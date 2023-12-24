996a0ecec5f99b4a2d7d44f6786382b2f0b527b1
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-24 21:14:29 +0500

    simple

---
+++ main.js
@@ -0,0 +1,7 @@
+let fullname = prompt(" ismingizni kiriting")
+let isMarrid = false;
+let birthyear = 2003;
+
+let res = `${isMarrid ? "2 boshli" : "Bo'ydoq"} ${fullname} hozir ${2023 - birthyear} yoshda. ${fullname} uylan endi eshshak`;
+
+alert(res);
